8f5e41c9706fb64cd036e2786cb3ac24ced1c73f
Latika04Parolkar <60360732+Latika04Parolkar@users.noreply.github.com>
2022-11-06 22:30:16 +0530

    changed links

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const GOOGLE_CLIENT_ID = '308376505977-rkianeftb388ss9tfk66j993d47ombpp.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_ID = '1054259671222-ga7ig2f1614hchemcul61nhp34eo72ku.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/google/callback",
+    callbackURL: "https://code-sessions-backend.onrender.com/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
